32f34eea671de86a46a0ddd1e2684c57ea32b285
Douglas Silva <douglasbritodev@gmail.com>
2026-07-23 22:33:37 -0300

    calculadora quase pronta

--- calculadora.py
+++ calculadora.py
@@ -4,5 +4,20 @@ num2 = int(input("Digite um segundo número: "))
 print("Seu primeiro número foi ",num)
 print("Seu segundo número foi ",num2)
 
-resultado = num + num2
-print("A soma dos números é ",resultado)
+print("Escolha uma Operação: 1- Soma, 2- Subtração, 3- Multiplicação, 4- Divisão")
+operacao = input("Digite o número ao lado: ")
+
+if operacao == "1":
+      resultado = num + num2
+      print("Resultado dos números é ",resultado)
+elif operacao == "2":
+          resultado = num - num2
+          print("Resultado dos números é ",resultado)
+elif operacao == "3":
+          resultado = num * num2
+          print("Resultado dos números é ",resultado)
+elif operacao == "4":
+          resultado = num / num2
+          print("Resultado dos números é ",resultado)
+else:
+      print("nenhuma opção válida")
