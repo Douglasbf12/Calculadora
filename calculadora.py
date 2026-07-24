8e1cca05b38336926c551f6796ffed396f63bdda
Douglas Silva <douglasbritodev@gmail.com>
2026-07-23 21:51:37 -0300

    Deixando o número inteiro

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-num = input("Digite um número: ")
-num2 = input("Digite um segundo número: ")
+num = int(input("Digite um número: "))
+num2 = int(input("Digite um segundo número: "))
 print("Seu primeiro número foi ",num)
 print("Seu segundo número foi ",num2)
