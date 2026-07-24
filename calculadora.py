bfc6c3d76b5a422f4855e864f805e9805f87c387
Douglas Silva <douglasbritodev@gmail.com>
2026-07-23 21:56:42 -0300

    Fazendo a soma dos dois números

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,8 @@
 num = int(input("Digite um número: "))
 num2 = int(input("Digite um segundo número: "))
+
 print("Seu primeiro número foi ",num)
 print("Seu segundo número foi ",num2)
+
+resultado = num + num2
+print("A soma dos números é ",resultado)
