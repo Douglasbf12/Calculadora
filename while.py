9de4f15c128d03b8d8c0f09f30e4aeaaa5283975
Douglas Silva <douglasbritodev@gmail.com>
2026-08-01 12:20:42 -0300

    Testando e Aprendendo a função While

---
+++ while.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome: ")
+cond = input("Deseja continuar? (s/n): ")
+while cond == "s":
+    nome = input("Digite seu nome: ")
+    cond = input("Deseja continuar? (s/n): ")
+print("Até mais, tchau tchau!")
