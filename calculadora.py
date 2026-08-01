c9587b2c5947981a32cc225177fb6826da3bd043
Douglas Silva <douglasbritodev@gmail.com>
2026-08-01 12:44:29 -0300

    Calculadora com Repetição

--- calculadora.py
+++ calculadora.py
@@ -1,26 +1,30 @@
-num = int(input("Digite um número: "))
-num2 = int(input("Digite um segundo número: "))
+cond = "s"
+while cond == "s":
+      num = int(input("Digite um número: "))
+      num2 = int(input("Digite um segundo número: "))
 
-print("Seu primeiro número foi ",num)
-print("Seu segundo número foi ",num2)
+      print("Seu primeiro número foi ",num)
+      print("Seu segundo número foi ",num2)
 
-print("Escolha uma Operação: 1- Soma, 2- Subtração, 3- Multiplicação, 4- Divisão")
-operacao = input("Digite o número ao lado: ")
+      print("Escolha uma Operação: 1- Soma, 2- Subtração, 3- Multiplicação, 4- Divisão")
+      operacao = input("Digite o número ao lado: ")
 
-if operacao == "1":
-      resultado = num + num2
-      print("Resultado dos números é ",resultado)
-elif operacao == "2":
-          resultado = num - num2
-          print("Resultado dos números é ",resultado)
-elif operacao == "3":
-          resultado = num * num2
-          print("Resultado dos números é ",resultado)
-elif operacao == "4":
-          if num2 == 0:
-                  print("O número não pode ser dividido por 0")
-          else:        
-                  resultado = num / num2
-                  print("Resultado dos números é ",resultado)
-else:
-      print("nenhuma opção válida")
+      if operacao == "1":
+            resultado = num + num2
+            print("Resultado dos números é ",resultado)
+      elif operacao == "2":
+            resultado = num - num2
+            print("Resultado dos números é ",resultado)
+      elif operacao == "3":
+            resultado = num * num2
+            print("Resultado dos números é ",resultado)
+      elif operacao == "4":
+            if num2 == 0:
+                print("O número não pode ser dividido por 0")
+            else:        
+                resultado = num / num2
+                print("Resultado dos números é ",resultado)
+      else:
+            print("nenhuma opção válida")
+      cond = input("Deseja fazer outra conta? (s/n): ")
+print("Até mais, tchau tchau!")
